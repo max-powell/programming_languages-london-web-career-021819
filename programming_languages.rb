9f596a15bb108cbc61f57ebbb04106701f8ad197
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-01 13:45:18 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, langs|
+    langs.each do |lang, detail|
+      if !(new_hash.keys.include?(lang))
+        new_detail = detail
+        new_detail[:style] = [style]
+        new_hash[lang] = new_detail
+      else
+        new_hash[lang][:style] << style
+      end
+    end
+  end
+  new_hash
 end
