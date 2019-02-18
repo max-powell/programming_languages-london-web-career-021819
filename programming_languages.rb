352da46a87a1c0a0f6a6408b8c00657e329ae444
Max Powell <max_f_powell@hotmail.co.uk>
2019-02-18 15:17:39 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,6 +2,7 @@ require "pry"
 
 def reformat_languages(languages)
   new_hash = {}
+<<<<<<< HEAD
   languages.each do |style, langs|
     langs.each do |lang, detail|
       if !(new_hash.keys.include?(lang))
@@ -14,4 +15,11 @@ def reformat_languages(languages)
     end
   end
   new_hash
+=======
+  languages.each do |type, langs|
+    langs.each do |lang, detail|
+      binding.pry
+    end
+  end
+>>>>>>> 79b646d204efe4f39b363ec8ae4c7d7ae256ddfa
 end
